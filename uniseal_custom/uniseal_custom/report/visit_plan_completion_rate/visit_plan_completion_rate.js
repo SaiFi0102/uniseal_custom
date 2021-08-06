08a3618a9bba169b2b1570294f0fd3db39194874
Saif Ur Rehman <saifi0102@gmail.com>
2021-08-06 16:33:40 +0500

    chore: Add new reports

---
+++ uniseal_custom/uniseal_custom/report/visit_plan_completion_rate/visit_plan_completion_rate.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Saif Ur Rehman and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Visit Plan Completion Rate"] = {
+	"filters": [
+
+	]
+};
---
+++ uniseal_custom/uniseal_custom/report/visit_productivity/visit_productivity.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Saif Ur Rehman and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Visit Productivity"] = {
+	"filters": [
+
+	]
+};
